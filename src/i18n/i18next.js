a8eb343d92e99839028e7a9b2ce9fada2e0cc63f
david-allan-jones <davej1094@yahoo.com>
2021-03-14 20:38:18 -0500

    Fix issue where non supported languages would load semantic names for strings.

--- src/i18n/i18next.js
+++ src/i18n/i18next.js
@@ -2,8 +2,11 @@ import i18next from 'i18next'
 import enLocale from './locales/en.json'
 import jaLocale from './locales/ja.json'
 
+const defaultLangauge = 'en'
+const supported = ['en', 'ja']
+
 i18next.init({
-    lng: navigator.language,
+    lng: supported.includes(navigator.language) ? navigator.language : defaultLangauge,
     resources: {
         en: { translation: enLocale },
         ja: { translation: jaLocale }
