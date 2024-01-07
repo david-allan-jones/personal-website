cf7f75c3c351125b5c154097a145c6037c876292
david-allan-jones <davej1094@yahoo.com>
2024-01-07 11:30:18 +0900

    Update routes to work with github pages

--- src/navigations.js
+++ src/navigations.js
@@ -4,9 +4,9 @@ import Projects from './Projects/Projects'
 import Contact from './Contact/Contact'
 
 const navigations = [
-    { label: i18next.t('navigation.home'), path: '/', component: <Home /> },
-    { label: i18next.t('navigation.projects'), path: '/projects', component: <Projects /> },
-    { label: i18next.t('navigation.contact'), path: '/contact', component: <Contact /> }
+    { label: i18next.t('navigation.home'), path: '/personal-website', component: <Home /> },
+    { label: i18next.t('navigation.projects'), path: '/personal-website/projects', component: <Projects /> },
+    { label: i18next.t('navigation.contact'), path: '/personal-website/contact', component: <Contact /> }
 ]
 
 export default navigations
